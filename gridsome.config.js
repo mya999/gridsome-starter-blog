2d342dbccbf250ecb2ddc16948b9925b15ecfbf5
mya9 <imohduni@gmail.com>
2020-04-06 22:59:14 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,8 +5,8 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Mohammed Y. Mardi',
+  siteDescription: 'Ph.D., Computer Science.',
 
   templates: {
     Post: '/:title',
